417500be06561c4ea6cadfa4816b24d213081127
Kevin Lewis <kevin@jellybabi.es>
2015-05-08 17:57:52 +0100

    Header slideshow

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+$("#fold").backgroundCycle({
+	imageUrls: [
+		"/assets/img/fold/1.jpg",
+		"/assets/img/fold/2.jpg",
+		"/assets/img/fold/3.jpg",
+		"/assets/img/fold/4.jpg",
+		"/assets/img/fold/5.jpg",
+		"/assets/img/fold/6.jpg",
+		"/assets/img/fold/7.jpg",
+		"/assets/img/fold/8.jpg",
+		"/assets/img/fold/9.jpg",
+		"/assets/img/fold/10.jpg",
+		"/assets/img/fold/11.jpg",
+	],
+	duration: 5000,
+	fadeSpeed: 1000
+});
