a8df61c59b31147aa95cba9160410838ff5bceb4
Kevin Lewis <kevin@jellybabi.es>
2015-05-08 19:59:01 +0100

    Scroll on bottom arrow, footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,18 @@ $("#fold").backgroundCycle({
 	duration: 5000,
 	fadeSpeed: 1000
 });
+
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 500, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
